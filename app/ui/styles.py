22d7d8c876938e12608b8bc74a39c20394086363
G <guilherme.borges@aluno.ifsp.edu.br>
2026-09-04 20:13:07 -0300

    corrige bug

--- app/ui/styles.py
+++ app/ui/styles.py
@@ -133,6 +133,7 @@ QPushButton#botaoPerigo:hover {
 
 QTableWidget {
     background-color: white;
+    color: #1c2733;
     border: 1px solid #dbe1e8;
     border-radius: 6px;
     gridline-color: #e7ebef;
